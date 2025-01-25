5b86ac3ec89048936b6eb05c3fb7e8949d0d3bdd
FrovaHappy <happypro5.0@gmail.com>
2025-01-25 13:44:26 -0300

    refactor: swap project details between Anime Hoshi and Beatriz ~ Dono

--- src/const/projects.ts
+++ src/const/projects.ts
@@ -15,40 +15,6 @@ export interface ProjectData {
 }
 const projects: ProjectData[] = [
   {
-    title: "Anime Hoshi (Archivado)",
-    description:
-      "Recibe notificaciones de las ultimas emisiones de anime, de tus paginas como CrunchyRoll, animeFlv y más.",
-    image: ["/images/projects/animehoshi.png"],
-    technologies: [
-      { name: "ScrapingWeb", color: "#000000" },
-      { name: "Typescript", color: "#000000" },
-      { name: "ReactJs", color: "#000000" },
-      { name: "NodeJs", color: "#000000" },
-      { name: "ExpressJs", color: "#000000" },
-      { name: "MongoOse", color: "#000000" },
-    ],
-    links: [
-      {
-        name: "Github",
-        icon: "IconBrandGithub",
-        url: "https://github.com/FrovaHappy/anime-hoshi",
-        color: "#000000",
-      },
-      {
-        name: "Website",
-        icon: "IconLink",
-        url: "https://animehoshi.xyz/",
-        color: "#000000",
-      },
-      {
-        name: "Docs",
-        icon: "IconFile",
-        url: "https://docs.animehoshi.xyz/",
-        color: "#000000",
-      },
-    ],
-  },
-  {
     title: "Beatriz ~ Dono",
     description:
       "Bot Multi-propósito, que actualmente cuenta con dos funciones principales /colors que permite cambiar el color del nombre y /welcome que personaliza las bienvenidas.",
@@ -83,6 +49,40 @@ const projects: ProjectData[] = [
         name: "Docs",
         icon: "IconFile",
         url: "https://frovahappy.gitbook.io/beatriz-bot-docs/",
+        color: "#000000",
+      },
+    ],
+  },
+  {
+    title: "Anime Hoshi (Archivado)",
+    description:
+      "Recibe notificaciones de las ultimas emisiones de anime, de tus paginas como CrunchyRoll, animeFlv y más.",
+    image: ["/images/projects/animehoshi.png"],
+    technologies: [
+      { name: "ScrapingWeb", color: "#000000" },
+      { name: "Typescript", color: "#000000" },
+      { name: "ReactJs", color: "#000000" },
+      { name: "NodeJs", color: "#000000" },
+      { name: "ExpressJs", color: "#000000" },
+      { name: "MongoOse", color: "#000000" },
+    ],
+    links: [
+      {
+        name: "Github",
+        icon: "IconBrandGithub",
+        url: "https://github.com/FrovaHappy/anime-hoshi",
+        color: "#000000",
+      },
+      {
+        name: "Website",
+        icon: "IconLink",
+        url: "https://animehoshi.xyz/",
+        color: "#000000",
+      },
+      {
+        name: "Docs",
+        icon: "IconFile",
+        url: "https://docs.animehoshi.xyz/",
         color: "#000000",
       },
     ],
